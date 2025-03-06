70dfb186c87b4826681d2a1856322e68cd4397e3
Vishal <95615776+vishal7245@users.noreply.github.com>
2025-03-07 00:10:49 +0530

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -3,7 +3,7 @@ import reflex as rx
 config = rx.Config(
     app_name="tcag",
     db_url="sqlite:///reflex.db",
-    api_url="http://tcaglifesciences.com:8003",
+    api_url="https://tcaglifesciences.com:8003",
     frontend_port=3003,
     backend_port=8003,
     cors_allowed_origins=["*"],
